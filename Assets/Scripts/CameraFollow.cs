0da033faf4a599c5a1ef2a5282c03e91da013a00
Slugazoid <albertsebastian2301@gmail.com>
2026-07-14 23:07:54 +0700

    Add Main Char & Animation Walk, Run, Jump

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,21 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     public float smoothSpeed = 0.9f;
 
+    public Camera cam;
+    public Transform LeftBound;
+    public Transform RightBound;
+
     private void LateUpdate()
     {
         Vector3 desiredPosition = playerTransform.position + offset;
+        float camHalfWidth = cam.orthographicSize * cam.aspect;
+        float clampedX = Mathf.Clamp(
+            desiredPosition.x,
+            LeftBound.position.x + camHalfWidth,
+            RightBound.position.x + camHalfWidth
+            );
+        desiredPosition.x = clampedX;
+
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D Rigidbody2D;
+    public Animator animator;
     public float speed;
     public float input;
     public float jumpForce;
+    public Transform leftBound;
+    public Transform rightBound;
+    public float boundOffset = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(input));
+        Vector3 currentScale = transform.localScale;
 
-        if (Input.GetButton("Jump"))
+        if (input > 0)
+        {
+            currentScale.x = Mathf.Abs(currentScale.x);
+        }
+        else if (input < 0)
+        {
+            currentScale.x = -Mathf.Abs(currentScale.x);
+        }
+
+        transform.localScale = currentScale;
+
+        if (Input.GetButtonDown("Jump"))
         {
             Rigidbody2D.linearVelocity = Vector2.up * jumpForce;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Mathf.Abs(Rigidbody2D.linearVelocity.y) < 0.01f)
+        {
+            animator.SetBool("isJumping", false);
         }
     }
 
     void FixedUpdate()
     {
         Rigidbody2D.linearVelocity = new Vector2(input * speed, Rigidbody2D.linearVelocity.y);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, leftBound.position.x + boundOffset, rightBound.position.x - boundOffset);
+        transform.position = pos;
     }
 }
